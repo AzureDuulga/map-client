b396406dba6f2d2acb6a2525e6383e6e0cfdfdc8
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2023-03-31 17:44:30 +0800

    map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
+import css from "./app.module.css";
+import { useState } from "react";
 
 function App() {
+  const [branches, setBranches] = useState([]);
+  const getAllBranch = async () => {
+    try {
+      const result = await axios.get("http://localhost:8001/restaurants");
+      setBranches(result?.data?.message);
+    } catch (err) {
+      console.log("ERR", err);
+    }
+  };
+  const getNearBranch = async () => {
+    try {
+      const result = await axios.post(
+        "http://localhost:8000/restaurants/near?distance=200",
+        {
+          lat: 47.92394060040875,
+          lon: 106.93371541130081,
+        }
+      );
+      setBranches(result.data.branches);
+    } catch (err) {
+      console.log("ERR", err);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={css.app}>
+      <h1>газрын зураг</h1>
+      <div>
+        <button onClick={getAllBranch}>Show all branches</button>
+        <button onClick={getNearBranch}>Show nearest branch</button>
+      </div>
+      <div className={css.map}>
+        <MapContainer
+          center={[47.923778067105836, 106.93411832087956]}
+          zoom={17}
+          scrollWheelZoom={true}
+          className={css.mapContainer}
         >
-          Learn React
-        </a>
-      </header>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {branches?.map((el) => {
+            console.log(el, "...");
+            return (
+              <Marker
+                key={el._id}
+                position={[
+                  el.location.coordinates[1],
+                  el.location.coordinates[0],
+                ]}
+              >
+                <Popup>{el.name}</Popup>
+              </Marker>
+            );
+          })}
+        </MapContainer>
+      </div>
     </div>
   );
 }
